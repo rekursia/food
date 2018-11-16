bd66735ea55395f7a0001d0d214e68a3490c8684
=Ирина Честнова <=i.chestnova@gmail.com>
2018-11-16 14:09:48 +0300

    reqired for allauth

--- mymenu/mymenu/settings.py
+++ mymenu/mymenu/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
     'rest_auth.registration',
 
     'rest_framework.authtoken'
